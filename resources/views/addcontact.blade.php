4d9ad46c87055d1ac80775d21f30456a8884a7b8
Saikat724 <talukdersaikat444@gmail.com>
2023-02-08 17:13:42 +0600

    Phone Book

---
+++ resources/views/addcontact.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="phonebook">
+        <h2>Add Contact</h2>
+        <form action="" method="post">
+            <label for="name">Add name </label> <br>
+            <input type="text" name="name"> <br>
+            <label for="number">Add Number</label> <br>
+            <input type="text" name="number"> <br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+    </div>
+</body>
+
+</html>
